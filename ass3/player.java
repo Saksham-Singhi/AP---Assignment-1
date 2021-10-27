89833fd3afa03cca4f956aacd0cc1d69b7e5ef4e
Saksham Singhi <82875816+Saksham-Singhi@users.noreply.github.com>
2021-10-27 22:07:29 +0530

    Update player.java

--- ass3/player.java
+++ ass3/player.java
@@ -22,42 +22,47 @@ class player {
             flr = 0;
             pts = 1;
             System.out.println("Player flr floor: " + flr+ "\r\n" + name + " has reached an Empty Floor \r\n" + "Total points: " + pts);
-            nextRoll();
+            nextMove();
         } else if (n == 2) {
             System.out.println("Game cannot start until you get 1");
             run();
         }
     }
+    
+    public int getroll() {
+	    dice dice = new dice();
+	    dice.roll();
+	    int n = dice.getfase();
+	    return n;
+    }
 
-    public void nextRoll() {
+    public void nextMove() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hit enter to roll the dice");
         sc.nextLine();
-        dice dice = new dice();
-        dice.roll();
-        int n = dice.getfase();
+        int n = getroll();
         System.out.println("dice gave " + n);
         if (flr + n <= 13) {
         flr = flr + n;
-        implementRoll();}
+        Move();}
         else {
         	System.out.print("Out of Bounds, Roll Again");
-        	nextRoll();
+        	nextMove();
         }
     }
 
-    public void implementRoll() {
+    public void Move() {
         String newflr = typ(flr);
 
         if (newflr.equals("e")) {
-            elevator ele = new elevator();
+            elevator Elev = new elevator();
             System.out.println("Player position Floor- " + flr+ "\r\n" + name + " has reached a elevator floor");
-            ele.setpts(pts);
-            pts = ele.addpts();
+            Elev.setpts(pts);
+            pts = Elev.addpts();
             System.out.println("total points: " + pts);
-            ele.setpos(flr);
-            flr = ele.addpos();
-            implementRoll();
+            Elev.setpos(flr);
+            flr = Elev.addpos();
+            Move();
         } else if (newflr.equals("l")) {
             ladder lad = new ladder();
             System.out.println("player position Floor- " + flr+ "\r\n" + name + " has reached a ladder floor");
@@ -66,7 +71,7 @@ class player {
             System.out.println("total points: " + pts);
             lad.setpos(flr);
             flr = lad.addpts();
-            implementRoll();
+            Move();
         } else if (newflr.equals("s")) {
             snake sna = new snake();
             System.out.println("Player position FLoor- " + flr+ "\r\n" + name + " has reached a snake floor");
@@ -75,7 +80,7 @@ class player {
             System.out.println("total points: " + pts);
             sna.setpos(flr);
             flr = sna.addpos();
-            implementRoll();
+            Move();
         } else if (newflr.equals("c")) {
             kingCobra cob = new kingCobra();
             System.out.println("Player position FLoor- " + flr + "\r\n" + name + " has reached a cobra floor");
@@ -84,14 +89,14 @@ class player {
             System.out.println("total points: " + pts);
             cob.setpos(flr);
             flr = cob.addpos();
-            implementRoll();
+            Move();
         } else if (newflr.equals("mt")) {
             System.out.println("player position Floor- " + flr+ "\r\n" + name + " has reached an Empty floor");
             pts = pts + 1;
             System.out.println("Total points: " + pts);
-            nextRoll();
+            nextMove();
         } else if (newflr.equals("f")) {
-		System.out.println("player position Floor- " + flr+ "\r\n" + name + " has reached The Final Floor"+ "\r\n" + "Total points: " + pts+ "\r\n" + "Game over" + "\r\n" + name + " accumulated " + pts + " points.");
+            System.out.println("player position Floor- " + flr+ "\r\n" + name + " has reached The Final Floor"+ "\r\n" + "Total points: " + pts+ "\r\n" + "Game over" + "\r\n" + name + " accumulated " + pts + " points.");
         }
     }
 
@@ -115,6 +120,7 @@ class player {
 	                return ("mt");
 	            }
 	        }
-        }
+        
     
 }
+
